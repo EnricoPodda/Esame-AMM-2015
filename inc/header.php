403a34241e0e3da007a9836ef81981b614d354a7
Enrico Podda <e.ipodda@googlemail.com>
2016-12-29 12:47:10 +0100

    Iniziata la parte utente

---
+++ inc/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Enrico Blog </title>
+    <link rel="stylesheet" type="text/css" href="./admin/css/global.css" />
+	<script type="text/javascript" src="./admin/js/jquery-1.8.min.js"></script>
+	<script type="text/javascript" src="./admin/js/bootstrap.min.js"></script>
+    
+</head>
+
+<body>
+<div class="top-bar">
+     
+    <div class="menu-left">
+    	<a href="index.php?page=home"> Home page</a>
+    </div>
+
+    <? if(isset($_SESSION['enrico-blog']['admin'])): ?>
+    <div class="menu-right">
+	    <a href="index.php?page=edit-profile&&username<?= $_SESSION['enrico-blog']['admin']['username'];?>">Modifica profilo</a>
+	    <a href="index.php?page=logout">Logout</a></li>
+
+	</div>
+	<? else: ?>
+		<div class="menu-right">
+			<a href="index.php?page=login">Login</a>
+		</div>
+	<? endif; ?>
+</div> <!-- ./ top-bar -->
