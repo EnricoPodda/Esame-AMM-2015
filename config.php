f0b82b903459718b1748ae67b959b45f54ab4742
Enrico Podda <e.ipodda@googlemail.com>
2017-01-04 12:36:12 +0100

    Bug fix.

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ $config['user'] = 'poddaEnrico';
 $config['pass'] = 'pappagallo7338';
 $config['db']   = 'amm15_poddaEnrico';
 
-$config['conn'] = mysql_connect ($config['host'], $config['user'], $config['pass']) or die ('Unable to connect to server, check the configuration file');
+$config['conn'] = mysql_connect ($config['host'], $config['user'], $config['pass']) or die ('Unable to connect to server, check the configuration file.............................');
                   mysql_select_db ($config['db'], $config['conn']) or die('Unable to connect to database, check the configuration file');
 						 
 $config['sql_config']	= "SELECT * FROM config";
